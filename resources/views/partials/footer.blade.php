caa312e69551a1fb894d10946aa405ce130efe0a
Trophysystems <129720010+Trophysystems@users.noreply.github.com>
2023-08-23 11:21:15 +0530

    NAme changes

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- ======= Footer ======= -->
 <footer id="footer" class="footer">
   <div class="copyright">
-    &copy; Copyright <strong><span>Register of Co-operative society</span></strong>. All Rights Reserved
+    &copy; Copyright <strong><span>Registrar of Co-operative societies</span></strong>. All Rights Reserved
   </div>
   <div class="credits">
     <!-- All the links in the footer should remain intact. -->
