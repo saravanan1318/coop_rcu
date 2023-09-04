f68c168b0f87217df778998b9a850633a144d7a8
Trophysystems <129720010+Trophysystems@users.noreply.github.com>
2023-09-04 12:10:42 +0530

    Daniel Changes 04092023

--- resources/views/deposit/annual/add.blade.php
+++ resources/views/deposit/annual/add.blade.php
@@ -18,7 +18,7 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-body">
-                  <h5 class="card-title">Target & outstanding Add</h5>
+                  {{--  <h5 class="card-title">Target & outstanding Add</h5>  --}}
                   <div class="row">
                     <div class="col-sm-12 col-md-12 mb-4">
                         @if(session('status'))
