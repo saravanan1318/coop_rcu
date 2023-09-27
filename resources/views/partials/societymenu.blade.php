fbae8f7d701c6075d63b860a6ad8dd2f926273b5
Naveen <102748623+naveen1134@users.noreply.github.com>
2023-09-27 16:27:29 +0530

    Naveen DRPDS building changes  added

--- resources/views/partials/societymenu.blade.php
+++ resources/views/partials/societymenu.blade.php
@@ -615,7 +615,7 @@
     </li><!-- End Dashboard Nav -->
     <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#build-nav" data-bs-toggle="collapse" href="#">
-          <i class="bi bi-journal-text"></i><span> Fair Shop Building</span><i class="bi bi-chevron-down ms-auto"></i>
+          <i class="bi bi-journal-text"></i><span> Fair Price Shop Building</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
         <ul id="build-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
