68dc26fc76a4ca9a2318c1fbca1eef9039aba634
logicEnvision <logicenvision@gmail.com>
2023-10-03 23:49:17 +0530

    change the super admin dashboar to dashboard updated by Mukesh team

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <main id="main" class="main">
         <div class="pagetitle">
-            <h1>Superadmin Dashboard</h1>
+            <h1>Dashboard</h1>
             <nav>
                 <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href="index.html">Home</a></li>
