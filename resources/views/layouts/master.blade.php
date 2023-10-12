fd3f686383b0139783d5e9255a9ef2b4dcb74940
MUKESHGA <mukesh@gravityalpha.com>
2023-10-13 00:49:31 +0530

    import target options pushed by Mukesh Team

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -255,5 +255,50 @@
             }
         });
     });
+    $('#generateSampleFile').on('click', function(e) {
+        e.preventDefault();
+        // Get the circle value from the input field
+        var circleValue = $('#circle').val();
+        var csrfToken = $('meta[name="csrf-token"]').attr('content');
+        if(circleValue =="")
+        {
+            alert("Please select circle ");
+
+        }
+        else
+        {
+            $.ajax({
+                type: 'POST',
+                url: '/jr/generatesampleFiles',
+                method: 'POST',
+                data: {circle: circleValue},
+                headers: {
+                    'X-CSRF-TOKEN': csrfToken // Use the csrfToken variable
+                },
+                success: function(response) {
+                    // Handle the response from the server
+                    console.log(response);
+                    // URL of the file to download
+                    const fileUrl = response;
+
+                    const downloadLink = document.createElement('a');
+                    downloadLink.href = fileUrl;
+                    downloadLink.target = '_blank'; // Open in a new tab/window (optional)
+                    // downloadLink.download = 'desired-filename.csv'; // Specify the desired filename (optional)
+
+// Trigger a click event on the anchor to initiate the download
+                    downloadLink.click();
+                },
+                error: function(xhr, status, error) {
+                    console.log(xhr.responseText);
+                    alert("Facing some technical issue ");
+                }
+            });
+
+        }
+
+        // Perform the AJAX request
+
+    });
 </script>
 </html>
